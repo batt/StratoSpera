96b3b103fc83525c1ff706051f0994381bac213c
Francesco Sacchi <batt@develer.com>
2011-05-05 01:27:40 +0200

    Start to refactor MMA845X driver.

--- bertos/drv/mma845x.c
+++ bertos/drv/mma845x.c
@@ -44,6 +44,70 @@
 
 #define MMA845x_DEV_ADDR 0x1C
 
+#define CTRL_REG1 0x2A
+#define CTRL_REG2 0x2B
+#define CTRL_REG3 0x2C
+#define CTRL_REG4 0x2D
+#define CTRL_REG5 0x2E
+
+
+static bool mma845x_writeReg(I2c *i2c, uint8_t dev_addr, uint8_t reg_addr, uint8_t data)
+{
+	dev_addr &= 1;
+	dev_addr = (dev_addr | MMA845x_DEV_ADDR) << 1;
+
+	i2c_start_w(i2c, dev_addr, 2, I2C_STOP);
+	i2c_putc(i2c, reg_addr);
+	i2c_putc(i2c, data);
+	return !i2c_error(i2c);
+}
+
+static int mma845x_readReg(I2c *i2c, uint8_t dev_addr, uint8_t reg_addr)
+{
+	uint8_t data;
+	dev_addr &= 1;
+	dev_addr = (dev_addr | MMA845x_DEV_ADDR) << 1;
+
+	i2c_start_w(i2c, dev_addr, 1, I2C_STOP);
+	i2c_putc(i2c, reg_addr);
+	i2c_start_r(i2c, dev_addr, 1, I2C_STOP);
+	i2c_read(i2c, &data, sizeof(data));
+
+	if (i2c_error(i2c))
+		return EOF;
+	else
+		return (int)(uint8_t)data;
+}
+
+#define ACTIVE_BIT BV(0)
+
+bool mma845x_enable(I2c *i2c, uint8_t addr, bool state)
+{
+	int ctrl_reg1 = mma845x_readReg(i2c, addr, CTRL_REG1);
+	if (ctrl_reg1 == EOF)
+		return false;
+
+	ctrl_reg1 &= ~ACTIVE_BIT;
+	ctrl_reg1 |= state ? ACTIVE_BIT : 0;
+	return mma845x_writeReg(i2c, addr, CTRL_REG1, ctrl_reg1);
+}
+
+#define DATARATE_SHIFT 3
+#define DATARATE_MASK (BV(DATARATE_SHIFT) | BV(DATARATE_SHIFT+1) | BV(DATARATE_SHIFT+2))
+
+bool mma845x_datarate(I2c *i2c, uint8_t addr, Mma845xDataRate rate)
+{
+	ASSERT(rate < MMADR_CNT);
+
+	int ctrl_reg1 = mma845x_readReg(i2c, addr, CTRL_REG1);
+	if (ctrl_reg1 == EOF)
+		return false;
+
+	ctrl_reg1 &= ~DATARATE_MASK;
+	ctrl_reg1 |= rate << DATARATE_SHIFT;
+	return mma845x_writeReg(i2c, addr, CTRL_REG1, ctrl_reg1);
+}
+
 bool mma845x_read(I2c *i2c, uint8_t addr, float *acc)
 {
 	uint8_t data[6];
@@ -69,4 +133,9 @@ bool mma845x_read(I2c *i2c, uint8_t addr, float *acc)
 	}
 
 	return true;
+}
+
+bool mma845x_init(I2c *i2c, uint8_t addr)
+{
+
 }
--- bertos/drv/mma845x.h
+++ bertos/drv/mma845x.h
@@ -4,6 +4,21 @@
 #include <drv/i2c.h>
 #include <cfg/compiler.h>
 
+typedef enum Mma845xDataRate
+{
+	MMADR_800HZ,
+	MMADR_400HZ,
+	MMADR_200HZ,
+	MMADR_100HZ,
+	MMADR_50HZ,
+	MMADR_12_5HZ,
+	MMADR_6_25HZ,
+	MMADR_1_56HZ,
+	MMADR_CNT,
+} Mma845xDataRate;
+
 bool mma845x_read(I2c *i2c, uint8_t addr, float *acc);
+bool mma845x_datarate(I2c *i2c, uint8_t addr, Mma845xDataRate rate);
+bool mma845x_enable(I2c *i2c, uint8_t addr, bool state);
 
 #endif /* DRV_MMA845X_H */
