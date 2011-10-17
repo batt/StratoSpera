cd39e3c247e95bc00268d1053544a03caf76a903
Francesco Sacchi <batt@develer.com>
2011-10-18 01:39:28 +0200

    Add AFSK1200 modem in python (RX only for now).

---
+++ scripts/afsk.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+
+from ax25 import AX25_ESC, HDLC_FLAG, HDLC_RESET
+from collections import deque
+
+SAMPLERATE = 9600
+BITRATE = 1200
+SAMPLE_PER_BIT = 8
+PHASE_BIT = 8
+PHASE_INC = 1
+PHASE_MAX = SAMPLE_PER_BIT * PHASE_BIT
+PHASE_THRES = PHASE_MAX / 2
+
+def bit_differ(bitline1, bitline2):
+    return (bitline1 ^ bitline2) & 0x01
+
+def edge_found(bitline):
+    return bit_differ(bitline, bitline >> 1)
+
+class Hdlc(object):
+    def __init__(self):
+        self.demod_bits = 0
+        self.rxstart = False
+        self.currchar = 0
+        self.bit_idx = 0
+
+    def parse(self, bit):
+        self.demod_bits <<= 1
+        self.demod_bits &= 0xFF
+        self.demod_bits |= 1 if bit else 0
+
+       	#HDLC Flag
+        if self.demod_bits == ord(HDLC_FLAG):
+            self.rxstart = True
+            self.currchar = 0
+            self.bit_idx = 0
+            return HDLC_FLAG
+
+	    #Reset
+        if (self.demod_bits & ord(HDLC_RESET)) == ord(HDLC_RESET):
+            self.rxstart = False
+            return ""
+
+        if not self.rxstart:
+            return ""
+
+        #Stuffed bit
+        if (self.demod_bits & 0x3f) == 0x3e:
+            return ""
+
+        if self.demod_bits & 0x01:
+            self.currchar |= 0x80
+
+        self.bit_idx += 1
+        out = ""
+        if self.bit_idx >= 8:
+            if self.currchar in (ord(HDLC_FLAG), ord(HDLC_RESET), ord(AX25_ESC)):
+                out += AX25_ESC
+
+            out += chr(self.currchar)
+
+            self.currchar = 0
+            self.bit_idx = 0
+        else:
+            self.currchar >>= 1
+
+        return out
+
+class Afsk(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+        #RX section
+        assert(SAMPLERATE == 9600)
+        assert(BITRATE == 1200)
+        assert(SAMPLE_PER_BIT == 8)
+
+        self.delay_fifo = deque([0] * (SAMPLE_PER_BIT / 2))
+        self.iir_x = [0] * 2
+        self.iir_y = [0] * 2
+        self.sampled_bits = 0
+        self.curr_phase = 0
+        self.found_bits = 0
+        self.hdlc = Hdlc()
+        self.out = deque()
+
+    def _processSample(self, sample):
+        sample = ord(sample) - 127
+
+        self.iir_x[0] = self.iir_x[1]
+        self.iir_x[1] = (self.delay_fifo.popleft() * sample) >> 2
+        #self.iir_x[1] = (self.delay_fifo.popleft() * sample) / 3.558147322
+        self.iir_y[0] = self.iir_y[1]
+        self.iir_y[1] = self.iir_x[0] + self.iir_x[1] + (self.iir_y[0] >> 1)
+        #self.iir_y[1] = self.iir_x[0] + self.iir_x[1] + self.iir_y[0] * 0.4379097269
+
+        self.sampled_bits <<= 1
+        self.sampled_bits |= 1 if self.iir_y[1] > 0 else 0
+
+        self.delay_fifo.append(sample)
+
+        if edge_found(self.sampled_bits):
+            if self.curr_phase < PHASE_THRES:
+                self.curr_phase += PHASE_INC
+            else:
+                self.curr_phase -= PHASE_INC
+        self.curr_phase += PHASE_BIT
+
+        if self.curr_phase >= PHASE_MAX:
+            self.curr_phase %= PHASE_MAX
+            self.found_bits <<= 1
+            self.found_bits &= 0xFF
+
+            self.sampled_bits &= 0x07
+
+            if self.sampled_bits in (0x07, 0x06, 0x05, 0x03):
+                self.found_bits |= 1
+            return self.hdlc.parse(not edge_found(self.found_bits))
+        else:
+            return ""
+
+
+    def read(self, size=1):
+        while len(self.out) < size:
+            for c in self.stream.read(512):
+                self.out.extend(self._processSample(c))
+
+        out = ""
+        for i in range(size):
+            out += self.out.popleft()
+
+        return out
+
+if __name__ == "__main__":
+    import pyaudio
+    import sys
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format = pyaudio.paUInt8,
+        channels = 1,
+        rate = SAMPLERATE,
+        input = True,
+        frames_per_buffer = 1024)
+    afsk = Afsk(stream)
+    sys.stdout.write(afsk.read(1024))
+    stream.close()
+    p.terminate()
+
+
---
+++ scripts/ax25.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+from crc_ccitt import *
+from collections import deque
+AX25_ESC = "\x1B"
+HDLC_FLAG = "\x7E"
+HDLC_RESET = "\x7F"
+
+AX25_CRC_CORRECT = 0xF0B8
+AX25_MIN_FRAME_LEN = 18
+AX25_CTRL_UI = 0x03
+AX25_PID_NOLAYER3 = 0xF0
+
+
+def decode_call(buf):
+    addr = ""
+    for i in range(6):
+        addr += chr(ord(buf.popleft()) >> 1)
+    addr = addr.strip() + "-" + str((ord(buf[0]) >> 1) & 0x0f)
+    return addr
+
+def decode_packet(buf):
+    msg = {}
+    msg["dst"] = decode_call(buf)
+    buf.popleft()
+    msg["src"] = decode_call(buf)
+    i = 0
+    while not (ord(buf.popleft()) & 0x01):
+        msg["rpt%d" % i] = decode_call(buf)
+        i += 1
+
+    msg["ctrl"] = ord(buf.popleft())
+
+    if msg["ctrl"] != AX25_CTRL_UI:
+        print "Only UI frames are handled, got [%02X]" % msg["ctrl"]
+        return
+
+    msg["pid"] = ord(buf.popleft())
+    if msg["pid"] != AX25_PID_NOLAYER3:
+        print "Only frames without layer3 protocol are handled, got [%02X]" % msg["pid"]
+        return
+
+    msg["data"] = "".join(buf)[:-2]
+    return msg
+
+class Ax25(object):
+    def __init__(self, stream):
+        self.stream = stream
+        self.escape = False
+        self.crc_in = CRC_CCITT_INIT_VAL;
+        self.sync = False
+        self.buf = deque()
+
+    def _process(self, c):
+        if not self.escape and c == HDLC_FLAG:
+            msg = None
+            if len(self.buf) >= AX25_MIN_FRAME_LEN:
+                if self.crc_in == AX25_CRC_CORRECT:
+                    msg = decode_packet(self.buf)
+
+            self.sync = True
+            self.crc_in = CRC_CCITT_INIT_VAL
+            self.buf = deque()
+            return msg
+
+
+        if not self.escape and c == HDLC_RESET:
+            print "HDLC reset"
+            self.sync = False
+            return
+
+        if not self.escape and c == AX25_ESC:
+            self.escape = True
+            return
+
+        if self.sync:
+            self.buf.append(c)
+            self.crc_in = updcrc_ccitt(c, self.crc_in)
+
+        self.escape = False
+
+    def recv(self, size = 1):
+        msg = []
+        while len(msg) < size:
+            m = self._process(self.stream.read(1))
+            if m:
+                msg.append(m)
+
+        return msg
+
+if __name__ == "__main__":
+    import pyaudio
+    import afsk
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format = pyaudio.paUInt8,
+        channels = 1,
+        rate = 9600,
+        input = True,
+        frames_per_buffer = 1024)
+    afsk = afsk.Afsk(stream)
+    ax25 = Ax25(afsk)
+    while 1:
+        print ax25.recv()
+
+    stream.close()
+    p.terminate()
+
+
