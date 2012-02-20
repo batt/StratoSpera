2ee2cae72e231894aa7b24ff8c20723c6c5ec6a7
Daniele Basile <asterix@develer.com>
2012-02-20 17:32:25 +0100

    Start bsm-radio prj.

---
+++ bsm-radio/bsm-radio_user.mk
@@ -0,0 +1,43 @@
+#
+# User makefile.
+# Edit this file to change compiler options and related stuff.
+#
+
+# Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
+bsm-radio_PROGRAMMER_TYPE = jtag-tiny
+bsm-radio_PROGRAMMER_PORT = none
+
+# Files included by the user.
+bsm-radio_USER_CSRC = \
+	$(bsm-radio_SRC_PATH)/main.c \
+	#
+
+# Files included by the user.
+bsm-radio_USER_PCSRC = \
+	#
+
+# Files included by the user.
+bsm-radio_USER_CPPASRC = \
+	#
+
+# Files included by the user.
+bsm-radio_USER_CXXSRC = \
+	#
+
+# Files included by the user.
+bsm-radio_USER_ASRC = \
+	#
+
+# Flags included by the user.
+bsm-radio_USER_LDFLAGS = \
+	#
+
+# Flags included by the user.
+bsm-radio_USER_CPPAFLAGS = \
+	#
+
+# Flags included by the user.
+bsm-radio_USER_CPPFLAGS = \
+	-fno-strict-aliasing \
+	-fwrapv \
+	#
