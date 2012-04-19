0a18dd1282ca6c4ba0d3572e3b0eded211b40691
Francesco Sacchi <batt@develer.com>
2012-04-19 23:04:42 +0200

    Properly set version name; automatically use the version name for the tag during release.

--- release.sh
+++ release.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#set -x
-set -e
-
-if [ ! $# = 1 ] ; then
-	echo "Usage:" $0 "<version_name>"
-	exit 1
-fi
+#set -e
 
 git diff --exit-code > /dev/null
 if [ $? -ne 0 ]; then
 	echo "ERROR: uncommitted changes!"
 	exit 2
 fi
-version=$1
+
+set -x
+
+version=`python findrev.py`
 echo "Current version will be \""$version"\", do you want to proceed?"
 read resp
 if [ $resp != y ]; then
