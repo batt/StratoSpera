bc0c8be236093766131cd6e805fb1a99f0d64cc6
Francesco Sacchi <batt@develer.com>
2012-04-25 12:42:04 +0200

    Fix compatibility issues with IE; better handling of local app configuration .

--- scripts/webserver/config.js
+++ scripts/webserver/config.js
@@ -1,3 +1,7 @@
+if (typeof local_app === 'undefined') {
+    local_app = true;
+}
+
 stsp_config = {
 	//Launch base coordinates used to compute distance
 	base_lat: 43.606167,
@@ -18,17 +22,17 @@ stsp_config = {
 		'Accel. X (m/s^2)',
 		'Accel. Y (m/s^2)',
 		'Accel. Z (m/s^2)',
-		'HADARP (cpm)',
+		'HADARP (cpm)'
 	],
 
 	//Delay between updates
 	upd_delay: 5000,
 
-	//opencycle_url: 'http://b.tile.opencyclemap.org/cycle',
-	//Use this for local cached maps (require custom webserver)
-	opencycle_url: 'maps',
+	//If app is in local use the local 'maps' directory for tiles
+	//otherwise use opencycle_url: 'http://b.tile.opencyclemap.org/cycle',
+	opencycle_url: local_app ? 'maps' : 'http://b.tile.opencyclemap.org/cycle',
 
 	//Set to true to have OpenCycle maps as the default map
 	//if set to false, the default will be Google Maps classic
-	opencycle_default: true,
+	opencycle_default: local_app
 };
