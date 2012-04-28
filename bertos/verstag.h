d6ff882b685325889c6d56c2424b215a5061f472
Francesco Sacchi <batt@develer.com>
2012-04-28 11:54:51 +0200

    Bump up version.

--- bertos/verstag.h
+++ bertos/verstag.h
@@ -51,7 +51,7 @@
 #define APP_COPYRIGHT "Copyright 2009 Develer (http://www.develer.com/)"
 
 #define VERS_NAME  "cygnus"
-#define VERS_REV   2
+#define VERS_REV   3
 
 /**
  * If _SNAPSHOT is defined, \c VERS_TAG contains the build date
