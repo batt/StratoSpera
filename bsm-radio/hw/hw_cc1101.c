e90f268db012d5614b811a9bcc9744208d1ab219
Daniele Basile <asterix@develer.com>
2012-05-21 18:37:01 +0200

    Specify to two configuration one for low power mode and the other for sensitivity rx.

--- bsm-radio/hw/hw_cc1101.c
+++ bsm-radio/hw/hw_cc1101.c
@@ -58,6 +58,11 @@ const Setting ping_low_baud_868[] =
   {CC1101_FIFOTHR,     0x47 /* RX FIFO and TX FIFO Thresholds */},
   {CC1101_PKTCTRL0,    0x05 /* Packet Automation Control */},
   {CC1101_PKTCTRL1,    0x00 /* Packet Automation Control */},
+  {CC1101_CHANNR,      0x07 /* Channel Number */},
+
+  {CC1101_PATABLE,     0xC5 /* Various Test Settings */},
+
+#if CONFIG_RADIO_CURRENT_OPT
   {CC1101_FSCTRL1,     0x06 /* Frequency Synthesizer Control */},
   {CC1101_FREQ2,       0x21 /* Frequency Control Word, High Byte */},
   {CC1101_FREQ1,       0x62 /* Frequency Control Word, Middle Byte */},
@@ -75,7 +80,29 @@ const Setting ping_low_baud_868[] =
   {CC1101_FSCAL0,      0x1F /* Frequency Synthesizer Calibration */},
   {CC1101_TEST2,       0x81 /* Various Test Settings */},
   {CC1101_TEST1,       0x35 /* Various Test Settings */},
+
+#else
+
+  {CC1101_FSCTRL1,     0x06 /* Frequency Synthesizer Control */},
+  {CC1101_FREQ2,       0x21 /* Frequency Control Word, High Byte */},
+  {CC1101_FREQ1,       0x62 /* Frequency Control Word, Middle Byte */},
+  {CC1101_FREQ0,       0x76 /* Frequency Control Word, Low Byte */},
+  {CC1101_MDMCFG4,     0xF5 /* Modem Configuration */},
+  {CC1101_MDMCFG3,     0x83 /* Modem Configuration */},
+  {CC1101_MDMCFG2,     0x13 /* Modem Configuration */},
+  {CC1101_DEVIATN,     0x15 /* Modem Deviation Setting */},
+  {CC1101_MCSM0,       0x18 /* Main Radio Control State Machine Configuration */},
+  {CC1101_FOCCFG,      0x16 /* Frequency Offset Compensation Configuration */},
+  {CC1101_WORCTRL,     0xFB /* Wake On Radio Control */},
+  {CC1101_FSCAL3,      0xE9 /* Frequency Synthesizer Calibration */},
+  {CC1101_FSCAL2,      0x2A /* Frequency Synthesizer Calibration */},
+  {CC1101_FSCAL1,      0x00 /* Frequency Synthesizer Calibration */},
+  {CC1101_FSCAL0,      0x1F /* Frequency Synthesizer Calibration */},
+  {CC1101_TEST2,       0x81 /* Various Test Settings */},
+  {CC1101_TEST1,       0x35 /* Various Test Settings */},
   {CC1101_TEST0,       0x09 /* Various Test Settings */},
+
+#endif
 
   { 0xff, 0xff },
 };
@@ -179,7 +206,7 @@ int radio_send(const void *buf, size_t len)
 		LOG_ERR("Tx radio not in idle..\n");
 		return RADIO_TX_ERR;
 	}
-		
+
 	//Flush the data in the fifo
 	status = cc1101_strobe(CC1101_SFTX);
 	LOG_INFO("FlushTx: Rdy[%d] St[%d] FifoAvail[%d]\n", UNPACK_STATUS(status));
